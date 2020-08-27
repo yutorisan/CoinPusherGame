c0e21963797b28addd95e5d9561959a972a430a7
suzuya <>
2020-08-28 00:34:39 +0900

    MedalInputerに対するエディタ拡張により、インスペクタからメダルを投入できるように

---
+++ Assets/Editor/MedalInputerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MedalPusher.Medal;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MedalInputer))]
+public class MedalInputerEditor : Editor
+{
+    int clones = 1;
+    private MedalInputer medalInputer;
+
+    private void OnEnable()
+    {
+        medalInputer = target as MedalInputer;
+    }
+
+
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var medals = EditorGUILayout.IntField("投入枚数", clones);
+        clones = medals;
+        if (GUILayout.Button("投入"))
+        {
+            medalInputer.GenerateMedal(medals, 5);
+        }
+    }
+}
