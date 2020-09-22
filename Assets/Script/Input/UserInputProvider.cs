c34c6eda6273cb5a98ff80a492ec69c70a9419ee
suzuya <>
2020-09-23 01:19:23 +0900

    Input/Itemを実装

---
+++ Assets/Script/Input/UserInputProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine.EventSystems;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace MedalPusher.Input
+{
+    public class UserInputProvider : IInputProvider
+    {
+        public UserInputProvider()
+        {
+            //InputObservable.FromKeyCode(KeyCode.Space)
+            //               .
+        }
+
+        public IObservable<KeyCode> ObservableInput => throw new NotImplementedException();
+    }
+}
---
+++ Assets/Script/Item/Checker/FieldItemChecker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace MedalPusher.Item.Checker
+{
+    public class FieldItemChecker : MonoBehaviour, IObservableItemChecker<IFieldItem>
+    {
+        public IObservable<IFieldItem> ItemChecked => throw new NotImplementedException();
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
