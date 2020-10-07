8319d9c0f2ff306f6a6607f5627698fd9a324072
suzuya <>
2020-10-07 22:55:48 +0900

    ボール抽選台を回転できるように

---
+++ Assets/Script/Lottery/LotteryBowlRotater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotteryBowlRotater : MonoBehaviour
+{
+    [SerializeField]
+    private float m_rotateSpeed = 1f;
+
+    private Rigidbody _rigidbody;
+    private float _rotateAngle = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        //_rigidbody.centerOfMass = new Vector3(0, 0, 1000);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.MoveRotation(Quaternion.Euler(0, _rotateAngle += m_rotateSpeed, 0));
+        //_rigidbody.rotation = Quaternion.Euler(0, ++_rotateAngle, 0);
+    }
+}
