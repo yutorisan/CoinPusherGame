737b0bca7b22fa33f92f4f99a21bc266cc03b9a5
yutori <>
2021-01-06 22:01:11 +0900

    SlotStarterを実装

--- Assets/Script/Slot/SlotStarter.cs
+++ Assets/Script/Slot/SlotStarter.cs
@@ -1,44 +1,45 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using MedalPusher.Slot.Stock;
 using UnityEngine;
 using Zenject;
+using UniRx;
 
 namespace MedalPusher.Slot
 {
     /// <summary>
     /// スロットの抽選を開始させる
     /// </summary>
-    public interface ISlotStarter
-    {
-        /// <summary>
-        /// スロット開始
-        /// </summary>
-        void SlotStart();
-    }
-    /// <summary>
-    /// スロットの抽選を開始させる
-    /// </summary>
-    public class SlotStarter : MonoBehaviour, ISlotStarter
+    public class SlotStarter : MonoBehaviour
     {
         [Inject]
         private ISlotRoleDeterminer _roleDeterminer;
+        [Inject]
+        private IObservableStockCount _observableStock;
+        [Inject]
+        private IObservableSlotStatus _slotStatus;
 
         // Start is called before the first frame update
         void Start()
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        public void SlotStart()
-        {
-            throw new NotImplementedException();
+            //ストックが追加された場合と、
+            _observableStock.Stock
+                            .Pairwise()
+                            //スロットが稼働中でない場合のみ
+                            .Where(_ => _slotStatus.Status.Value == SlotStatus.Idol)
+                            //前よりストックが増えている
+                            .Where(pair => pair.Current > pair.Previous)
+                            .AsUnitObservable()
+                            .Merge(
+            //ストックが溜まっている場合にIdol状態になった場合に、
+                _slotStatus.Status
+                           .Where(status => status == SlotStatus.Idol)
+                           .Where(_ => _observableStock.Stock.Value > 0)
+                           .AsUnitObservable())
+            //スロットを回転させる（役の決定を依頼する）
+                           .Subscribe(_ => _roleDeterminer.DetermineRole());
+                                  
         }
     }
 }
