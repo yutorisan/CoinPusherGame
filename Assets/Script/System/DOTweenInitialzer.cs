be3b8e023b8ea1a8e12a7de82b257592cee0db48
yutori <>
2021-01-12 01:17:42 +0900

    DOTweenが自動再生されないように設定 専用の設定ファイルを追加

---
+++ Assets/Script/System/DOTweenInitialzer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class DOTweenInitialzer : MonoBehaviour
+{
+    private void Awake()
+    {
+        //DOTweenのSequenceやTweenを生成したときに自動生成されないように設定
+        DOTween.Init();
+        DOTween.defaultAutoPlay = AutoPlay.None;
+    }
+}
