23f103aec739fb8c3e16651799acffcbb0875842
yutorisan <60573963+yutorisan@users.noreply.github.com>
2021-03-04 22:34:32 +0900

    UniTaskとDOTweenの拡張メソッド追加 ObservableExのパス変更

---
+++ Assets/Script/System/Extensions/DOTweenEx/DOTweenEx.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using DG.Tweening;
+
+public static class DOTweenEx
+{
+    public static Sequence ToSequence(this Tween tween) =>
+        DOTween.Sequence().Append(tween);
+}
---
+++ Assets/Script/System/Extensions/UniTaskEx/UniTaskEx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+
+public static class UniTaskEx
+{
+    public static UniTask ContinueWithIf(this UniTask task, bool condition, Func<UniTask> continueTask)
+    {
+        if (!condition) return task;
+        else return task.ContinueWith(continueTask);
+    }
+    public static UniTask WhenAll(this IEnumerable<UniTask> source) => UniTask.WhenAll(source);
+}
