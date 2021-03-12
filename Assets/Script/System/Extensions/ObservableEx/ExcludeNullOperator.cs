d3c68cc6fcd187d7d47909762d7b86679734577c
yutorisan <60573963+yutorisan@users.noreply.github.com>
2021-03-12 23:15:57 +0900

    RxのExcludeNullオペレータを追加

---
+++ Assets/Script/System/Extensions/ObservableEx/ExcludeNullOperator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UniRx;
+
+public static partial class ObservableEx
+{
+    public static IObservable<T> ExcludeNull<T>(this IObservable<T> source) =>
+        source.Where(n => n != null);
+}
