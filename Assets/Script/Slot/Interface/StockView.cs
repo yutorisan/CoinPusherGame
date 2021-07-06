c7bd0b254532283e8a4ea8d8f78fca6f4545be67
yutorisan <60573963+yutorisan@users.noreply.github.com>
2021-07-06 16:53:57 +0900

    Sceneにストックインジケータ表示用のSpriteオブジェクトを配置

---
+++ Assets/Script/Slot/Interface/StockView.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MedalPusher.Slot.Internal.Stock
+{
+    /// <summary>
+    /// 現在のストックの状況をユーザーに提示する
+    /// </summary>
+    public class StockView : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
